80a6d6d45d902ffdcf46f9c82c2376280979d5d9
Glenn Hickey <hickey@soe.ucsc.edu>
2012-05-30 11:11:55 -0700

    use sonlib to get resources

--- benchmarks/benchMark.py
+++ benchmarks/benchMark.py
@@ -10,45 +10,25 @@ import os
 import sys
 import traceback
 import time
-import subprocess
 
-from procWatch import monitor
 from sonLib.bioio import getTempDirectory
 from sonLib.bioio import getTempFile
+from sonLib.bioio import popenCatch
 from sonLib.bioio import system
 
+
 def runExecutable(command):
-    process = subprocess.Popen(command,
-                               shell=True,
-                               stdout=subprocess.PIPE,
-                               stderr=sys.stderr,
-                               bufsize=-1)
-
-    class Arg:
-        def __init__(self):
-            pass
-    procWatchArgs = Arg()
-    procWatchArgs.pids = [process.pid]
-    procWatchArgs.delay = 5.0
-    procWatchArgs.cmds = [command]
-    monitor(procWatchArgs)
-
-    # below should be unecessary since monitor waits out process.
-    # hopefully it can catch errors?
-    sts = process.wait()
-
-    if sts != 0:
-        raise RuntimeError("Command: %s exited with non-zero status %i" %
-                           (command, sts))
-    
-    return sts
+    cmdLine = "./runAndGetResources.py \'%s\'" % command
+    output = popenCatch(cmdLine)
+    values = output[1:-2].split(',')
+    return (float(values[0]), float(values[1]), float(values[2]))
     
 def runHalGen(preset, hdf5Chunk, hdf5Compression, outPath):
-    runExecutable("halRandGen --preset %s --hdf5Chunk %d\
+    return runExecutable("halRandGen --preset %s --hdf5Chunk %d\
     --hdf5Compression %d %s" % (preset, hdf5Chunk, hdf5Compression, outPath))
 
 def runHalCons(halPath, outputPath):
-    runExecutable("halCons %s > outputPath" % halPath)
+    return runExecutable("halCons %s > outputPath" % halPath)
 
 def initOptions():
     parser = argparse.ArgumentParser(description='Run an experiment.')
@@ -61,8 +41,15 @@ def initOptions():
 def main(argv=None):
     if argv is None:
         argv = sys.argv
+
+    parser = argparse.ArgumentParser(description='Get resources of executable')
+    parser.add_argument('--string', type=str,
+                        help='string')
+    parser.add_argument('--int', type=int, default=50, 
+                        help='int. default=%(default)s')
     parser = initOptions()
     args = parser.parse_args()
+    rval = 0
 
     try:
         tempDir = getTempDirectory(rootDir="./")
@@ -70,15 +57,15 @@ def main(argv=None):
     except:
         traceback.print_exc(file=sys.stdout)
         exit(1)
-
     try:
-        runHalGen("medium", 10000000, 5, tempFile)
-        runHalCons(tempFile, getTempFile(rootDir=tempDir))
+        print runHalGen("large", 10000000, 5, tempFile)
+        print runHalCons(tempFile, getTempFile(rootDir=tempDir))
     except:
         traceback.print_exc(file=sys.stdout)
-        exit(1)
+        rval = 1
 
     system("rm -rf %s" % tempDir)
-    
+    return rval
+
 if __name__ == "__main__":
     sys.exit(main())
---
+++ benchmarks/runAndGetResources.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+#Copyright (C) 2012 by Glenn Hickey
+#
+#Released under the MIT license, see LICENSE.txt
+
+
+import os
+import sys
+import time
+
+from sonLib.bioio import popenCatch
+from sonLib.bioio import getTotalCpuTimeAndMemoryUsage
+from sonLib.bioio import system
+            
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    if len(argv) != 2:
+        print "usage: runAndGetResources.py \'cmdline\'"
+        exit(1)
+    cmdline = argv[1]
+    wallStart = time.time()
+    output = popenCatch(cmdline)
+    wallClock = time.time() - wallStart
+    print (wallClock,) + getTotalCpuTimeAndMemoryUsage()
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
