c9ab1f8e356efc9352eb61f6774c38924b89c3b8
Glenn Hickey <hickey@soe.ucsc.edu>
2012-10-11 08:42:55 -0700

    add some comments

--- api/inc/halSequenceIterator.h
+++ api/inc/halSequenceIterator.h
@@ -13,19 +13,32 @@
 namespace hal {
 
 /** 
- * Interface for top segment iterator exposes the top segment
- * interface and some new methods for jumping around the genome.  
- * Always hidden in smart pointers in the public interface. 
+ * Iterate over sequences in the genome.  
  */
 class SequenceIterator
 {
 public:
+   /** Create a duplicate iterator referring to the same sequence
+    * which itself is not copied */
    virtual SequenceIteratorPtr copy() = 0;
+
+   /** Create a duplicate iterator referring to the same sequence
+    * which itself is not copied */
    virtual SequenceIteratorConstPtr copy() const = 0;
+
+   /** Move iterator to next sequence in the genome */
    virtual void toNext() const = 0;
+
+   /** Move iterator to previous sequence in the genome */
    virtual void toPrev() const = 0;
+   
+   /** Return pointer to the sequence */
    virtual Sequence* getSequence() = 0;
+
+   /** Return pointer to the sequence */
    virtual const Sequence* getSequence() const = 0;
+
+   /** Test if iterator points to same sequence as other iterator */
    virtual bool equals(SequenceIteratorConstPtr p2) const = 0;
 
 protected:
