e2cb689dccbed36d164975f02ac5352064e67107
Glenn Hickey <hickey@soe.ucsc.edu>
2012-10-19 14:30:34 -0700

    assert revisited

--- maf/impl/halMafWriteGenomes.cpp
+++ maf/impl/halMafWriteGenomes.cpp
@@ -295,7 +295,8 @@ void MafWriteGenomes::initBlockInfo(size_t col)
 #ifndef NDEBUG
         StartMap::const_iterator mapIt = startMap.find(row._startPosition);
         assert(mapIt != startMap.end());
-        assert(rowInfo._arrayIndex - mapIt->second._index <
+        assert(mapIt->second._count == 0 ||
+               rowInfo._arrayIndex - mapIt->second._index <
                mapIt->second._count);
 #endif
       }
