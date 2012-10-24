0589ecb3368dd0d81111f77ac985e46aac9ed256
Glenn Hickey <hickey@soe.ucsc.edu>
2012-10-24 10:38:20 -0700

    framework for liftover class

--- liftover/impl/halLiftover.cpp
+++ liftover/impl/halLiftover.cpp
@@ -10,3 +10,78 @@
 
 using namespace std;
 using namespace hal;
+
+Liftover::Liftover() : _inputFile(NULL), _outputFile(NULL),
+                       _srcGenome(NULL), _tgtGenome(NULL)
+{
+
+}
+
+Liftover::~Liftover()
+{
+
+}
+
+void Liftover::convert(AlignmentConstPtr alignment,
+                       const Genome* srcGenome,
+                       ifstream* inputFile,
+                       const Genome* tgtGenome,
+                       ofstream* outputFile)
+{
+  _srcGenome = srcGenome;
+  _inputFile = inputFile;
+  _tgtGenome = tgtGenome;
+  _outputFile = outputFile;
+  _missedSet.clear();
+  _tgtSet.clear();
+  assert(_srcGenome && _inputFile && tgtGenome && outputFile);
+
+  _tgtSet.insert(tgtGenome);
+
+  while (!inputFile->bad() && !inputFile->eof())
+  {
+    readBedLine();
+    _srcSequence = _srcGenome->getSequence(_inName);
+    if (_srcSequence == NULL)
+    {
+      pair<set<string>::iterator, bool> result = _missedSet.insert(_inName);
+      if (result.second == true)
+      {
+        std::cerr << "Unable to find sequence " << _inName << " in genome "
+                  << srcGenome->getName() << endl;
+      }
+    }
+    
+    else if (_inEnd >= _srcSequence->getSequenceLength())
+    {
+      std::cerr << "Skipping interval with endpoint " << _inEnd 
+                << "because sequence " << _inName << " has length " 
+                << _srcSequence->getSequenceLength() << endl;
+    }
+    else
+    {
+      liftInterval();
+    }
+  }
+}
+
+void Liftover::liftInterval()
+{
+  
+  _colIt = _srcSequence->getColumnIterator(&_tgtSet, 0, _inStart, _inEnd - 1);
+  do 
+  {
+    
+  } while (_colIt->lastColumn() == false);
+  
+}
+
+void Liftover::readBedLine()
+{
+
+}
+
+void Liftover::writeBedLine()
+{
+
+}
--- liftover/inc/halLiftover.h
+++ liftover/inc/halLiftover.h
@@ -7,12 +7,55 @@
 #ifndef _HALLIFTOVER_H
 #define _HALLIFTOVER_H
 
-#include <list>
-#include <map>
-#include <set>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
 #include "hal.h"
 
 namespace hal {
+
+class Liftover
+{
+public:
+   
+   Liftover();
+   virtual ~Liftover();
+
+   void convert(AlignmentConstPtr alignment,
+                const Genome* srcGenome,
+                std::ifstream* inputFile,
+                const Genome* tgtGenome,
+                std::ofstream* outputFile);
+                   
+protected:
+
+   void readBedLine();
+   void writeBedLine();
+   void liftInterval();
+
+   AlignmentConstPtr _alignment;
+   std::ifstream* _inputFile;
+   std::ofstream* _outputFile;
+   
+   // current bed line
+   std::string _inName;
+   hal_size_t _inStart;
+   hal_size_t _inEnd;
+   std::vector<std::string> _data;
+
+   hal_size_t _outStart;
+   hal_size_t _outEnd;
+
+   const Genome* _srcGenome;
+   const Genome* _tgtGenome;
+   const Sequence* _srcSequence;
+   std::set<const Genome*> _tgtSet;
+
+   ColumnIteratorConstPtr _colIt;
+
+   std::set<std::string> _missedSet;
+};
 
 }
 #endif
