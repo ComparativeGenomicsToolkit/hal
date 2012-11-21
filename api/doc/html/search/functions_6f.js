b37e119919bf21e78575905c4326251c12f4e3a3
Glenn Hickey <hickey@soe.ucsc.edu>
2012-11-20 17:30:26 -0800

    update html manual

--- api/doc/html/search/functions_6f.js
+++ api/doc/html/search/functions_6f.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['open',['open',['../classhal_1_1_alignment.html#abd75c81df12ab829feacd72b4ab1d933',1,'hal::Alignment::open(const std::string &amp;alignmentPath, bool readOnly)=0'],['../classhal_1_1_alignment.html#a723a690603fc14585d9d603250248e93',1,'hal::Alignment::open(const std::string &amp;alignmentPath) const =0']]],
   ['opengenome',['openGenome',['../classhal_1_1_alignment.html#a2ef53e44b6d9fa50279c25e4b80227d8',1,'hal::Alignment::openGenome(const std::string &amp;name) const =0'],['../classhal_1_1_alignment.html#ac7f1be0111fc005cd6eeb964e9d1bd31',1,'hal::Alignment::openGenome(const std::string &amp;name)=0']]],
-  ['openhalalignment',['openHalAlignment',['../namespacehal.html#ae53d20b3279aff57dc4668a91b0ce780',1,'hal']]],
-  ['openhalalignmentreadonly',['openHalAlignmentReadOnly',['../namespacehal.html#a0e1812c971eba1094a0634d3291310d6',1,'hal']]],
+  ['openhalalignment',['openHalAlignment',['../namespacehal.html#a8cec382b66b75fe3c25cf994619435f5',1,'hal']]],
+  ['openhalalignmentreadonly',['openHalAlignmentReadOnly',['../namespacehal.html#af7fa18d1488baff3f5eb786f36ec586c',1,'hal']]],
   ['overlaps',['overlaps',['../classhal_1_1_segment.html#aea6d5d0be2f936029e68d03cb31cc171',1,'hal::Segment']]]
 ];
