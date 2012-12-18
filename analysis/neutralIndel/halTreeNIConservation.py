16b93d987ccfe9405b8084997bbcc98839272a51
Glenn Hickey <hickey@soe.ucsc.edu>
2012-12-18 14:19:59 -0800

    debug NIConservation

--- analysis/neutralIndel/halTreeNIConservation.py
+++ analysis/neutralIndel/halTreeNIConservation.py
@@ -21,6 +21,17 @@ from hal.mutations.impl.halTreeMutations import getHalRootName
 from hal.mutations.impl.halTreeMutations import getHalParentName
 from hal.mutations.impl.halTreeMutations import getHalChildrenNames
 
+def checkFiles(background, mutations):
+    if not os.path.isfile(background):
+        raise RuntimeError("Background selection file %s not found. Make "
+                           "sure halTreeNIBackground has been run "
+                           "and that the paths are correctly "
+                           "specified" % background)
+    if not os.path.isfile(mutations):
+        raise RuntimeError("Mutations file %s not found.  Make sure that "
+                           "halTreeMutations has been run amd that the "
+                           "paths are correctly specified" % mutations)
+    
 def getHalTreeConservation(halPath, args, events, rootName=None):
     root = rootName
     if root is None:
@@ -28,12 +39,13 @@ def getHalTreeConservation(halPath, args, events, rootName=None):
     for child in getHalChildrenNames(halPath, root):
         bgFile = os.path.join(args.workDir, args.backgroundBedName % child)
         muFile = os.path.join(args.workDir, args.mutationsBedName % child)
-        outFile = os.path.join(args.workDir, args.conservedBedName % child)
+        checkFiles(bgFile, muFile)
+        outPath = os.path.join(args.workDir, args.conservedBedName % child)
+        outFile = open(outPath, "w")
         bc = BedConservation()
-        bc.computeBackgroundRate(args.bgFile, args.muFile, events)
-        bc.identifyConservedIntervals(args.mutationsBed, outFile, args.pval)
+        bc.computeBackgroundRate(muFile, bgFile, events)
+        bc.identifyConservedIntervals(muFile, outFile, args.pval)
         getHalTreeConservation(halPath, args, events, child)
-
         
 def main(argv=None):
     if argv is None:
@@ -48,7 +60,7 @@ def main(argv=None):
                         "background bed files where genome name is "
                         "specified as %%s.  Computed using halTreeNIBackground")
     parser.add_argument("--mutationsBedName", type=str,
-                        default="%%s_bg.bed", help="Name function for "
+                        default="%%s.bed", help="Name function for "
                         "background bed files where genome name is "
                         "specified as %%s.  Computed using halTreeMutations")
     parser.add_argument("--conservedBedName", type=str,
@@ -63,10 +75,10 @@ def main(argv=None):
                         help="max pval of conserved segment")
     
     args = parser.parse_args()
+    args.backgroundBedName = args.backgroundBedName.replace("%%", "%")
+    args.mutationsBedName = args.mutationsBedName.replace("%%", "%")
+    args.conservedBedName = args.conservedBedName.replace("%%", "%")
     events =  args.events.split()
-
-#    if not os.path.exists(args.workDir):
-#        os.makedirs(args.workDir)
         
     getHalTreeConservation(args.hal, args, events, args.root)
     
