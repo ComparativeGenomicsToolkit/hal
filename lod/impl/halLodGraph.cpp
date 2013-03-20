5fe1811511ffb0ca0dbbd38da8093cc9baba42c4
Glenn Hickey <hickey@soe.ucsc.edu>
2013-03-20 13:36:39 -0700

    add LodGraph class

---
+++ lod/impl/halLodGraph.cpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2013 by Glenn Hickey (hickey@soe.ucsc.edu)
+ *
+ * Released under the MIT license, see LICENSE.txt
+ */
+
+#include <cassert>
+#include "halLodNode.h"
+#include "halLodEdge.h"
+#include "halLodGraph.h"
+
+using namespace std;
+using namespace hal;
+
+LodGraph::LodGraph()
+{
+
+}
+
+LodGraph::~LodGraph()
+{
+  erase();
+}
+
+void LodGraph::erase()
+{
+  for (GenomeNodesIterator i = _genomeNodes.begin(); i != _genomeNodes.end();
+       ++i)
+  {
+    NodeList* nodeList = i->second;
+    for (NodeIterator j = nodeList->begin(); j != nodeList->end(); ++j)
+    {
+      delete *j;
+    }
+    delete nodeList;
+    _genomeNodes.erase(i);
+  }
+  _children.clear();
+  _parent = NULL;
+  _alignment = AlignmentConstPtr();
+}
+
+void LodGraph::build(AlignmentConstPtr alignment, const Genome* parent,
+                     const vector<const Genome*> children, 
+                     hal_size_t step)
+{
+  erase();
+  _alignment = alignment;
+  _parent = parent;
+  _children = children;
+  _step = step;
+
+  assert(_parent != NULL);
+  assert(_alignment->openGenome(_parent->getName()) == _parent);
+  
+  _genomeNodes.insert(pair<const Genome*, NodeList*>(_parent, new NodeList()));
+  for (size_t i = 0; i < _children.size(); ++i)
+  {
+    _genomeNodes.insert(pair<const Genome*, NodeList*>(_children[i], 
+                                                       new NodeList()));
+  }
+
+  for (GenomeNodesIterator gni = _genomeNodes.begin(); 
+       gni != _genomeNodes.end(); ++gni)
+  {
+    scanGenome(gni->first, gni->second);
+  }
+
+  _adjTable.writeAdjacenciesIntoNodes();
+}
+
+
+void LodGraph::scanGenome(const Genome* genome, NodeList* nodeList)
+{
+  // should be done at outside scope.
+  set<const Genome*> tgtSet;
+  for (GenomeNodesIterator gni = _genomeNodes.begin(); 
+       gni != _genomeNodes.end(); ++gni)
+  {
+    tgtSet.insert(gni->first);
+  }
+
+  SequenceIteratorConstPtr seqIt = genome->getSequenceIterator();
+  SequenceIteratorConstPtr seqEnd = genome->getSequenceEndIterator();
+  for (; seqIt != seqEnd; seqIt->toNext())
+  {
+    const Sequence* sequence = seqIt->getSequence();
+    hal_size_t len = sequence->getSequenceLength();
+    hal_index_t seqStart = sequence->getStartPosition();
+    for (hal_index_t pos = 0; pos < (hal_index_t)len; pos += (hal_index_t)_step)
+    {
+      // clamp to last position
+      if (pos > 0 && pos + (hal_index_t)_step >= (hal_index_t)len)
+      {
+        pos =  (hal_index_t)len - 1;
+      }
+      
+      // better to move column iterator rather than getting each time?
+      ColumnIteratorConstPtr colIt = sequence->getColumnIterator(&tgtSet, 0,
+                                                                 pos);
+      // convert pos to genome coordinate
+      LodNode* node = new LodNode(sequence, seqStart + pos, seqStart + pos);
+      _adjTable.addNode(node, colIt);
+    }
+  }
+}
--- lod/inc/halLodGraph.h
+++ lod/inc/halLodGraph.h
@@ -13,33 +13,51 @@
 #include <vector>
 #include <map>
 #include "hal.h"
+#include "halLodAdjTable.h"
 
 namespace hal {
 
 class LodNode;
-class LodEdge;
 
 class LodGraph
 {
 public:
    
-   typedef std::multiset<LodNode*, LodNodePLess> NodeSet;
-   typedef NodeSet::iterator NodeIterator;
-   typedef std::map<const Sequence*, NodeSet*> NodeRefMap;
-   typedef NodeRefMap::iterator NodeRefMapIterator;
-   typedef std::map<const Genome*, NodeSet*> NodeMap;
-   typedef NodeMap::iterator NodeMapIterator;
+   LodGraph();
+   ~LodGraph();
+
+   /** Build the LOD graph for a given subtree of the alignment.  The
+    * entire graph is stored in memory in a special structure (ie not within
+    * HAL).  The step parameter dictates how coarse-grained the interpolation
+    * is:  every step bases are sampled.  */
+   void build(AlignmentConstPtr alignment, const Genome* parent,
+              const std::vector<const Genome*> children, 
+              hal_size_t step);
 
 protected:
 
+   typedef std::list<LodNode*> NodeList;
+   typedef NodeList::iterator NodeIterator;
+   typedef std::map<const Genome*, NodeList*> GenomeNodes;
+   typedef GenomeNodes::iterator GenomeNodesIterator;
+
+   void erase();
+   void scanGenome(const Genome* genome, NodeList* nodeList);
+protected:
    
    // input alignment structure
    AlignmentConstPtr _alignment;
    const Genome* _parent;
    std::vector<const Genome*> _children;
 
-   NodeRefMap _nodeRefMap;
-   NodeMap _nodeMap;
+   // step size for interpolation
+   hal_size_t _step;
+
+   // ordered list of nodes for each sequence
+   GenomeNodes _genomeNodes;
+
+   // adjacency table used to generate the graph
+   LodAdjTable _adjTable;
 };
 
 }
