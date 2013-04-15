186f7b62c1091327c9b0eafdc0b990727d824f10
Glenn Hickey <hickey@soe.ucsc.edu>
2013-04-15 11:05:06 -0700

    update lodBenchmark to do a 2nd mafcomparator as well as a scan benchmark

--- lod/halLodBenchmark.py
+++ lod/halLodBenchmark.py
@@ -15,6 +15,7 @@ import os
 import sys
 import copy
 import subprocess
+import time
 from collections import defaultdict
 
 from hal.stats.halStats import runShellCommand
@@ -52,10 +53,19 @@ def compMaf(inHalPath, outDir, step, overwrite, doMaf):
     xmlPath = makePath(inHalPath, outDir, step, "comp", "xml")
     sumPath = makePath(inHalPath, outDir, step, "comp", "txt")
     if doMaf and (overwrite or not os.path.isfile(xmlPath)):
-        runShellCommand("mafComparator --maf1 %s --maf2 %s --out %s" % (
+        runShellCommand("mafComparator --maf1 %s --maf2 %s --out %s --samples 100000" % (
             srcMaf, tgtMaf, xmlPath))
         runShellCommand("comparatorSummarizer.py --xml %s > %s " % (xmlPath,
                                                                     sumPath))
+    xmlNearPath = makePath(inHalPath, outDir, step, "comp_near", "xml")
+    sumNearPath = makePath(inHalPath, outDir, step, "comp_near", "txt")
+    if doMaf and (overwrite or not os.path.isfile(xmlNearPath)):
+        runShellCommand(
+            "mafComparator --maf1 %s --maf2 %s --out %s --near %d --samples 100000" % (
+                srcMaf, tgtMaf, xmlNearPath, int(step)))
+        runShellCommand("comparatorSummarizer.py --xml %s > %s " % (
+            xmlNearPath, sumNearPath))
+
 
 def getPrecisionRecall(inHalPath, outDir, step, doMaf):
     if doMaf:
@@ -66,12 +76,39 @@ def getPrecisionRecall(inHalPath, outDir, step, doMaf):
         line = sumFile.next()
         tokens = line.split()
         assert tokens[2] == "self)"
-        return [float(tokens[3]), float(tokens[4])]
-    else:
-        return [0., 0.]
 
+        sumNearPath = makePath(inHalPath, outDir, step, "comp_near", "txt")
+        sumNearFile = open(sumNearPath, "r")
+        line = sumNearFile.next()
+        line = sumNearFile.next()
+        line = sumNearFile.next()
+        tokensNear = line.split()
+        assert tokensNear[2] == "self)"
+
+        return [float(tokens[3]), float(tokens[4]),
+                float(tokensNear[3]), float(tokensNear[4])]
+    elif step == 0:
+        return [1., 1., 1., 1.]
+    else:
+        return [0., 0., 0., 0.]
+
+def getScanTime(inHalPath, outDir, step):
+    srcHalPath = inHalPath
+    if step > 0:
+        srcHalPath = makePath(inHalPath, outDir, step, "lod", "hal")
+    genomes = getHalGenomes(inHalPath)
+    assert len(genomes) > 1
+    genName = genomes[1]
+    bedPath = makePath(inHalPath, outDir, step, genName, "bed")
+    print (step, bedPath)
+    t1 = time.time()
+    runShellCommand("halBranchMutations %s %s --refFile %s" % (
+        srcHalPath, genName, bedPath))
+    elapsedTime = time.time() - t1
+    return [elapsedTime]
+    
 def printTable(table):
-    print "Step, kb,, nTop,, nBottom,, Prec., Recall"
+    print "Step, kb,, nTop,, nBottom,, Prec., Recall, PrecNear, RecallNear, ScanTime"
     for step, data in sorted(table.items()):
         line = "%d" % step
         idx = 0
@@ -89,11 +126,13 @@ def printTable(table):
 def runSteps(inHalPath, outDir, steps, overwrite, doMaf):
     table = defaultdict(list)
     makeMaf(inHalPath, outDir, 0, overwrite, doMaf)
-    for step in steps:
-        table[0] = [os.path.getsize(inHalPath) / 1024]
-        table[0] += list(getHalTotalSegments(inHalPath))
-        table[0] += getPrecisionRecall(inHalPath, outDir, 0, False)
-    
+
+    table[0] = [os.path.getsize(inHalPath) / 1024]
+    table[0] += list(getHalTotalSegments(inHalPath))
+    table[0] += getPrecisionRecall(inHalPath, outDir, 0, False)
+    table[0] += getScanTime(inHalPath, outDir, 0)
+
+    for step in steps:    
         outPath = makePath(inHalPath, outDir, step, "lod", "hal")
         
         if overwrite is True or not os.path.isfile(outPath):
@@ -105,6 +144,7 @@ def runSteps(inHalPath, outDir, steps, overwrite, doMaf):
         table[step] = [os.path.getsize(outPath) / 1024]
         table[step] += list(getHalTotalSegments(outPath))
         table[step] += getPrecisionRecall(inHalPath, outDir, step, doMaf)
+        table[step] += getScanTime(inHalPath, outDir, step)
 
     return table
 
