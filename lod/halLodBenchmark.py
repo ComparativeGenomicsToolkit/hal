3166af8eddbcc243e89b283511a01b9941c1e5ea
Glenn Hickey <hickey@soe.ucsc.edu>
2013-04-20 10:56:02 -0700

    remove maxrefgap from hal2maf to speed up benchmark

--- lod/halLodBenchmark.py
+++ lod/halLodBenchmark.py
@@ -39,8 +39,7 @@ def makeMaf(inHalPath, outDir, step, overwrite, doMaf):
         srcHalPath = makePath(inHalPath, outDir, step, "lod", "hal")
     outMafPath = makePath(inHalPath, outDir, step, "out", "maf")
     if doMaf and (overwrite or not os.path.isfile(outMafPath)):
-        runShellCommand("hal2maf %s %s --maxRefGap 10" % (srcHalPath,
-                                                           outMafPath))
+        runShellCommand("hal2maf %s %s" % (srcHalPath, outMafPath))
 
 def compMaf(inHalPath, outDir, step, overwrite, doMaf):
     srcMaf = makePath(inHalPath, outDir, 0, "out", "maf")
