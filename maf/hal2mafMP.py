d07ddd5c8d95043713bc3b1b10eeef9c9d943f51
Glenn Hickey <hickey@soe.ucsc.edu>
2013-06-12 10:45:51 -0700

    revise parallel hal2maf interface to be simpler

--- maf/hal2mafMP.py
+++ maf/hal2mafMP.py
@@ -16,6 +16,8 @@ import subprocess
 import time
 import inspect
 import math
+import random
+import string
 from collections import defaultdict
 from multiprocessing import Pool
 
@@ -56,49 +58,60 @@ def getHal2MafCmd(options):
 # Generate a MAF file name from an input HAL file and some options
 # the MAF file will be in the specified directory
 def makeOutMafPath(options):
-    halFile = os.path.basename(options.halFile)
-    halName = os.path.splitext(halFile)[0]
+    mafFile = os.path.basename(options.mafFile)
+    mafName = os.path.splitext(mafFile)[0]
+    mafDir = os.path.dirname(options.mafFile)
     if options.smallFile:
-        halName += '_small'
+        mafName += '_small'
     else:
-        if options.refSequence is not None:
-            halName += '_%s' % options.refSequence
+        if options.refSequence is not None and options.splitBySequence is True:
+            mafName += '_%s' % options.refSequence
         if options.sliceNumber is not None:
-            halName += str(options.sliceNumber)
-    mafPath = os.path.join(options.mafDir, halName + '.maf')
+            mafName += '_%s_%d' % (options.tempID, options.sliceNumber)
+    mafPath = os.path.join(mafDir, mafName + '.maf')
     return mafPath
 
 # slice an interval
 # return (start, length, sliceIndex)
 def computeSlices(options, seqLen):
-    if seqLen > 0:
-        if options.sliceSize is None or options.sliceSize >= seqLen:
-            yield (0, seqLen, None)
+    inStart = options.start
+    if inStart is None:
+        inStart = 0
+    inLength = options.length
+    if inLength is None or inLength < 1:
+        inLength = seqLen
+    if inLength > 0:
+        if options.sliceSize is None or options.sliceSize >= inLength:
+            yield (inStart, inLength, None)
         else:
-            for i in xrange(seqLen / options.sliceSize):
-                yield (i * options.sliceSize, options.sliceSize, i)
-            r = seqLen % options.sliceSize
+            for i in xrange(inLength / options.sliceSize):
+                yield (inStart + i * options.sliceSize, options.sliceSize, i)
+            r = inLength % options.sliceSize
             if r > 0:
-                i = seqLen / options.sliceSize
-                yield (i * options.sliceSize, r, i)
+                i = inLength / options.sliceSize
+                yield (inStart + i * options.sliceSize, r, i)
 
-def concatenateSlices(options, sliceCmds):
-    if options.mafFile is not None:
-        first = True
-        for cmd in sliceCmds:
-            sliceMafPath = cmd.split()[2] #oops
+def concatenateSlices(sliceOpts, sliceCmds):
+    assert len(sliceOpts) > 0
+    assert len(sliceOpts) == len(sliceCmds)
+    if (sliceOpts[0].sliceSize is not None):
+        for opt, cmd in zip(sliceOpts, sliceCmds):
+            first = opt.sliceNumber == 0            
+            sliceMafPath = makeOutMafPath(opt)
             assert os.path.isfile(sliceMafPath)
+            sliceNum = opt.sliceNumber
+            opt.sliceNumber = None
+            outMafPath = makeOutMafPath(opt)
+            opt.sliceNumber = sliceNum
             if first:
-                os.rename(sliceMafPath, options.mafFile)
-                first = False
+                os.rename(sliceMafPath, outMafPath)
             else:
-                with open(options.mafFile, "a") as tgt:
+                with open(outMafPath, "a") as tgt:
                     with open(sliceMafPath, "r") as src:
                         for line in src:
                             if not line[0] == '#':
                                 tgt.write(line)
                 os.remove(sliceMafPath)
-
             
 # Decompose HAL file into slices according to the options then launch
 # hal2maf in parallel processes. 
@@ -110,8 +123,9 @@ def runParallelSlices(options):
     options.smallFile = False
     options.firstSmallFile = True
     sliceCmds = []
+    sliceOpts = []
     # we are going to deal with sequence coordinates
-    if options.sliceBySequence is True or options.refSequence is not None:
+    if options.splitBySequence is True or options.refSequence is not None:
         for sequence, seqLen, nt, nb in refSequenceStats:
             if options.refSequence is None or sequence == options.refSequence:
                 seqOpts = copy.deepcopy(options)
@@ -125,28 +139,35 @@ def runParallelSlices(options):
                     seqOpts.length = sLen
                     seqOpts.sliceNumber = sIdx
                     sliceCmds.append(getHal2MafCmd(seqOpts))
+                    sliceOpts.append(copy.deepcopy(seqOpts))
                 if seqOpts.smallFile is True and seqLen > 0:
                     options.firstSmallFile = False
     # we are slicing the gnome coordinates directly
     else:
         seqOpts = copy.deepcopy(options)
+        assert seqOpts.splitBySequence is False
         genomeLen = getHalGenomeLength(seqOpts.halFile, refGenome)
+        # auto compute slice size from numprocs
+        if seqOpts.sliceSize == None and seqOpts.numProc > 1:
+            refLen = genomeLen
+            if seqOpts.length is not None and seqOpts.length > 0:
+                refLen = seqOpts.length
+            seqOpts.sliceSize = refLen / seqOpts.numProc
+                
         index = 0
         for sStart, sLen, sIdx in computeSlices(seqOpts, genomeLen):
             seqOpts.start = sStart
             seqOpts.length = sLen
             seqOpts.sliceNumber = sIdx
             sliceCmds.append(getHal2MafCmd(seqOpts))
+            sliceOpts.append(copy.deepcopy(seqOpts))
             
-
     # run in parallel
     runParallelShellCommands(sliceCmds, options.numProc)
 
     # concatenate into output if desired
-    concatenateSlices(options, sliceCmds)
+    concatenateSlices(sliceOpts, sliceCmds)
 
-
-    
 
 def main(argv=None):
     if argv is None:
@@ -157,31 +178,28 @@ def main(argv=None):
         description="Multi-Process wrapper for hal2maf.")
 
     parser.add_argument("halFile", help="Input HAL file")
-    parser.add_argument("mafDir", help="Output directory for created MAF files")
-    parser.add_argument("numProc",
-                        help="Maximum number of processes to create",
-                        type=int)
+    parser.add_argument("mafFile", help="Output MAF file")
 
+    parser.add_argument("--numProc",
+                        help="Maximum number of processes to create.  If "
+                        " neither --sliceSize or --splitBySequence are "
+                        " specified, then the reference genome will be "
+                        "sliced to require --numProc jobs",
+                        type=int, default=1)
     parser.add_argument("--sliceSize",
-                        help="Length of slice along reference genome for each "
-                        "output MAF.", type=int,
+                        help="Maximum slice of reference sequence to process "
+                        "in a single process.", type=int,
                         default=None)
-    parser.add_argument("--sliceBySequence",
+    parser.add_argument("--splitBySequence",
                         help="Create an output MAF for each sequence in the "
-                        "reference genome.  Note that the --sliceSize option, "
-                        "if specifed, output MAF.", action="store_true",
+                        "reference genome. Output files will have the format "
+                        "mafFile_sequenceName.maf", action="store_true",
                         default=False)
     parser.add_argument("--smallSize",
-                        help="If --sliceBySequence is used, then all sequences "
+                        help="If --splitBySequence is used, then all sequences "
                         "with length less than smallSize will be lumped into "
-                        "a single output MAF called \"small\"",
+                        "a single output MAF called \"mafFile_small.maf\"",
                         type=int, default=0)
-    parser.add_argument("--mafFile",
-                        help="Concentate all output MAFs in the mafDir into "
-                        "combined MAF file in this path.  The split MAFs are"
-                        " deleted in the process.",
-                        default=None)
-
 
     ##################################################################
     #HDF5 OPTIONS (as copied from hal/api/hdf5_impl/hdf5CLParser.cpp)
@@ -262,23 +280,23 @@ def main(argv=None):
 
     if not os.path.isfile(args.halFile):
         raise RuntimeError("Input hal file %s not found" % args.halFile)
-    if not os.path.isdir(args.mafDir):
-        os.makedirs(args.mafDir)
-    if args.sliceBySequence:
+    assert args.mafFile != args.halFile
+    test = open(args.mafFile, "w")
+    test.write("\n")
+    test.close()
+    if args.splitBySequence:
         if args.start is not None:
-            raise RuntimeError("--sliceBySequence option currently "
+            raise RuntimeError("--splitBySequence option currently "
                                "incompatible with --start option")
         if args.length is not None:
-            raise RuntimeError("--sliceBySequence option currently "
+            raise RuntimeError("--splitBySequence option currently "
                                "incompatible with --length option")
     if args.sliceSize is not None and args.smallSize >= args.sliceSize:
         raise RuntimeError("--smallSize must be less than --sliceSize")
-    if args.mafFile is not None:
-        assert args.mafFile != args.halFile
-        test = open(args.mafFile, "w")
-        test.write("\n")
-        test.close()
 
+    # make a little id tag for temporary maf slices
+    S = string.ascii_uppercase + string.digits
+    args.tempID = 'hal2mafTemp' + ''.join(random.choice(S) for x in range(5))
     runParallelSlices(args)
     
 if __name__ == "__main__":
