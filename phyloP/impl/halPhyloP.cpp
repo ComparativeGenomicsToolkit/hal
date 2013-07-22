1346498a30895167a73f9c18497d67e1376c369e
Glenn Hickey <hickey@soe.ucsc.edu>
2013-07-22 15:54:17 -0700

    refactor phyloP class substantially to adopt hal coding style and to prepare for bed subselection support

--- phyloP/impl/halPhyloP.cpp
+++ phyloP/impl/halPhyloP.cpp
@@ -1,33 +1,124 @@
+/*
+ * Copyright (C) 2013 by Glenn Hickey (hickey@soe.ucsc.edu) and 
+ * Melissa Jane Hubisz (Cornell University)
+ *
+ * Released under the MIT license, see LICENSE.txt
+ */
+
 #include "halPhyloP.h"
 
 using namespace std;
 using namespace hal;
 
-halPhyloP::halPhyloP(hal::AlignmentConstPtr alignment, std::string modFile) {
-  _softMaskDups = 1;  //use soft mask by default
-  _maskAllDups = 0;  // only mask ambiguous bases in dups by default
-  _alignment = alignment;
-  _mode = CONACC;  // this tests for conservation and acceleration, I expect this is the only mode UCSC will want to use, but is easy to switch to CON or ACC or NNEUT
+PhyloP::PhyloP() : _mod(NULL), _softMaskDups(false), _maskAllDups(false),
+                   _seqnameHash(NULL), _colfitdata(NULL), _mode(CONACC),
+                   _msa(NULL)
+{
+  
+}
 
+PhyloP::~PhyloP() 
+{
+  clear();
+}
+
+void PhyloP::clear()
+{ 
+  if (_colfitdata != NULL)
+  {
+    col_free_fit_data(_colfitdata);
+  }
+  if (_msa != NULL)
+  {
+    msa_free(_msa);
+  }
+  if (_seqnameHash != NULL)
+  {
+    hsh_free(_seqnameHash);
+  }
+  _targetSet.clear();
+
+  // need to free _mod?
+
+}
+
+void PhyloP::init(AlignmentConstPtr alignment, const string& modFilePath,
+                  ostream* outStream,
+                  bool softMaskDups, 
+                  const string& dupType,
+                  const string& phyloPMode)
+{
+  clear();
+  _alignment = alignment;
+  _softMaskDups = (int)softMaskDups;
+  _outStream = outStream;
+
+  // set float precision to 3 places to be consistent with non-hal phyloP
+  _outStream->setf(ios::fixed, ios::floatfield);
+  _outStream->precision(3);
+
+  if (dupType == "ambiguous")
+  {
+    _maskAllDups = 1;
+  }
+  else if (dupType == "all")
+  {
+    _maskAllDups = 0;
+  }
+  else
+  {
+    throw hal_exception("unknown dupType + " + dupType + 
+                        ", should be all or ambiguous");
+  }
+
+  if (phyloPMode == "CONACC")
+  {
+    _mode = CONACC;
+  }
+  else if (phyloPMode == "CON") 
+  {
+    _mode = CON;
+  } 
+  else if (phyloPMode == "ACC") 
+  {
+    _mode = ACC;
+  }
+  else if (phyloPMode == "NNEUT") 
+  {
+    _mode = NNEUT;
+  }
+  else 
+  {
+    throw hal_exception("unknown phyloP mode " + phyloPMode);
+  }
+  
   //read in neutral model
-  FILE *infile = phast_fopen(modFile.c_str(), "r");
+  FILE *infile = phast_fopen(modFilePath.c_str(), "r");
   _mod = tm_new_from_file(infile, TRUE);
   phast_fclose(infile);
 
-  //make sure all species in the tree are in the alignment, otherwise print warning and prune tree; create targetSet from these species. Make a hash of species names to species index (using phast's hash structure)
+  // make sure all species in the tree are in the alignment, otherwise print
+  // warning and prune tree; create targetSet from these species.
+  // Make a hash of species names to species index (using phast's hash
+  // structure)
   int numspec=0;
   List *leafNames = tr_leaf_names(_mod->tree);
   int numleaf = lst_size(leafNames);
   List *pruneNames = lst_new_ptr(numleaf);
   _seqnameHash = hsh_new(numleaf*10);
   char **names = (char**)smalloc(numleaf * sizeof(char*));
-  for (int i=0; i < lst_size(leafNames); i++) {
-    string targetName = std::string(((String*)lst_get_ptr(leafNames, i))->chars);
+  for (int i=0; i < lst_size(leafNames); i++) 
+  {
+    string targetName = string(((String*)lst_get_ptr(leafNames, i))->chars);
     const Genome *tgtGenome = _alignment->openGenome(targetName);
-    if (tgtGenome == NULL) {
-      cerr << "Genome" << targetName << " not found in alignment; pruning from tree" << endl;
+    if (tgtGenome == NULL) 
+    {
+      cerr << "Genome" << targetName 
+           << " not found in alignment; pruning from tree" << endl;
       lst_push(pruneNames, lst_get_ptr(leafNames, i));
-    } else {
+    } 
+    else 
+    {
       String *leafName = (String*)lst_get_ptr(leafNames, i);
       hsh_put_int(_seqnameHash, leafName->chars, numspec);
       _targetSet.insert(tgtGenome);
@@ -40,9 +131,12 @@ halPhyloP::halPhyloP(hal::AlignmentConstPtr alignment, std::string modFile) {
   lst_free(leafNames);
   lst_free(pruneNames);
 
-  //create a dummy alignment with a single column. As we iterate through the columns we will fill in the bases and compute the phyloP scores for individual columns.
+  //create a dummy alignment with a single column. As we iterate through the 
+  // columns we will fill in the bases and compute the phyloP scores for
+  // individual columns.
   char **seqs = (char**)smalloc(numspec * sizeof(char*));
-  for (int i=0; i < numspec; i++) {
+  for (int i = 0; i < numspec; i++) 
+  {
     seqs[i] = (char*)smalloc(2*sizeof(char));
     seqs[i][0] = 'N';
     seqs[i][1] = '\0';
@@ -52,93 +146,196 @@ halPhyloP::halPhyloP(hal::AlignmentConstPtr alignment, std::string modFile) {
   msa_free_seqs(_msa);
   _colfitdata = col_init_fit_data(_mod, _msa, ALL, _mode, FALSE);
   _colfitdata->tupleidx = 0;
-
-}
-				  
-halPhyloP::~halPhyloP() {
-  col_free_fit_data(_colfitdata);
-  msa_free(_msa);
-  hsh_free(_seqnameHash);
 }
 
-// set whether we want to use hard or soft masks for dups. Hard mask will turn the entire column to N's (and therefore p-value to 1) if any species is duplicated. Soft mask will turn just that species to N. I expect we only want soft mask, which is default.
-void halPhyloP::setDupMask(string dupMask) {
-  if (dupMask == "hard") {
-    _softMaskDups=0;
-  } else if (dupMask == "soft") {
-    _softMaskDups=1;
-  } else {
-    throw hal_exception("unknown dupMask + " + dupMask + ", should be hard or soft");
+/** Given a Sequence (chromosome) and a (sequence-relative) coordinate
+ * range, print the phyloP wiggle with respect to the genomes
+ * in the target set 
+ *
+ * This is basically copied from halAlignability.cpp but returns
+ * the phyloP score rather than the count of alignments.
+ *
+ * Coordinates are always genome-relative by default (as opposed to 
+ * sequence-relative).  The one exception is all methods within the
+ * Sequence interface. 
+ *
+ * By default, all bases in the reference genome are scanned.  And all
+ * other genomes are considered.  The --refSequence, --start, and 
+ * --length options can limit the query to a subrange.  Note that unless
+ * --refSequence is specified, --start is genome-relative (based on 
+ * all sequences being concatenated together).
+ *
+ * Genomes to include are determined by genomes in the neutral model 
+ * (.mod) file- this should be in the format outputted by phyloFit.
+ *
+ * There are a few options for dealing with duplications. The default
+ * is dupMask=soft, dupType=ambiguous. This replaces any species base 
+ * with an N if duplications cause uncertainty in the base for a 
+ * particular alignment column. If dupType=all, then all duplications
+ * are masked regardless of whether they cause uncertainty. If
+ * dupMask=hard, then the entire column is masked (p-value returned is 1.0)
+ *
+ * Print the phyloP wiggle for a subrange of a given sequence to
+ * the output stream. */
+void PhyloP::processSequence(const Sequence* sequence,
+                             hal_index_t start,
+                             hal_size_t length,
+                             hal_size_t step)
+{
+  hal_size_t seqLen = sequence->getSequenceLength();
+  if (seqLen == 0)
+  {
+    return;
+  }
+  /** If the length is 0, we do from the start position until the end
+   * of the sequence */
+  if (length == 0)
+  {
+    length = seqLen - start;
+  }
+  hal_size_t last = start + length;
+  if (last > seqLen)
+  {
+    stringstream ss;
+    ss << "Specified range [" << start << "," << length << "] is"
+       << "out of range for sequence " << sequence->getName() 
+       << ", which has length " << seqLen;
+    throw (hal_exception(ss.str()));
+  }
+
+  const Genome* genome = sequence->getGenome();
+  string sequenceName = sequence->getName();
+  string genomeName = genome->getName();
+
+  /** The ColumnIterator is fundamental structure used in this example to
+   * traverse the alignment.  It essientially generates the multiple alignment
+   * on the fly according to the given reference (in this case the target
+   * sequence).  Since this is the sequence interface, the positions
+   * are sequence relative.  Note that we must specify the last position
+   * in advance when we get the iterator.  This will limit it following
+   * duplications out of the desired range while we are iterating. */
+  hal_size_t pos = start;
+  ColumnIteratorConstPtr colIt = 
+     sequence->getColumnIterator(&_targetSet,
+                                 0, pos,
+                                 last - 1,
+                                 true);
+
+  // note wig coordinates are 1-based for some reason so we shift to right
+  *_outStream << "fixedStep chrom=" << sequenceName << " start=" << start + 1
+              << " step=" << step << "\n";
+  
+  /** Since the column iterator stores coordinates in Genome coordinates
+   * internally, we have to switch back to genome coordinates.  */
+  // convert to genome coordinates
+  pos += sequence->getStartPosition();
+  last += sequence->getStartPosition();
+  while (pos <= last)
+  {
+    /** ColumnIterator::ColumnMap maps a Sequence to a list of bases
+     * the bases in the map form the alignment column.  Some sequences
+     * in the map can have no bases (for efficiency reasons) */ 
+    const ColumnIterator::ColumnMap* cmap = colIt->getColumnMap();
+    double pval = this->pval(cmap);
+
+    *_outStream << pval << '\n';
+    
+    /** lastColumn checks if we are at the last column (inclusive)
+     * in range.  So we need to check at end of iteration instead
+     * of beginning (which would be more convenient).  Need to 
+     * merge global fix from other branch */
+    if (colIt->lastColumn() == true)
+    {
+      break;
+    }
+
+    pos += step;    
+    if (step == 1)
+    {
+      /** Move the iterator one position to the right */
+      colIt->toRight();
+      
+      /** This is some tuning code that will probably be hidden from 
+       * the interface at some point.  It is a good idea to use for now
+       * though */
+      // erase empty entries from the column.  helps when there are 
+      // millions of sequences (ie from fastas with lots of scaffolds)
+      if (pos % 1000 == 0)
+      {
+        colIt->defragment();
+      }
+    }
+    else
+    {
+      /** Reset the iterator to a non-contiguous position */
+      colIt->toSite(pos, last);
+    }
   }
 }
-
-// set whether we want to consider anything a duplication if there are multiple alignments for one species, or if we only care about duplications which cause uncertainty in the bases (this is the default) 
-void halPhyloP::setDupType(string dupType) {
-  if (dupType == "ambiguous") {
-    _maskAllDups = 0;
-  } else if (dupType == "all") {
-    _maskAllDups = 1;
-  } else {
-    throw hal_exception("unknown dupType + " + dupType + ", should be all or ambiguous");
-  }
-}
-
-void halPhyloP::setPhyloPMode(string mode) {
-  mode_type newmode;
-  if (mode == "CONACC") {
-    newmode = CONACC;
-  } else if (mode == "CON") {
-    newmode = CON;
-  } else if (mode == "ACC") {
-    newmode = ACC;
-  } else if (mode == "NNEUT") {
-    newmode = NNEUT;
-  } else {
-    throw hal_exception("unknown phyloP mode " + mode);
-  }
-  if (_mode != newmode) {
-    col_free_fit_data(_colfitdata);
-    _mode = newmode;
-    _colfitdata = col_init_fit_data(_mod, _msa, ALL, _mode, FALSE);
-  }
-}
-
 
 // compute phyloP score for a particular alignment column, return pval
-double halPhyloP::pval(const ColumnIterator::ColumnMap *cmap) {
+double PhyloP::pval(const ColumnIterator::ColumnMap *cmap) 
+{
   for (int i=0; i < _msa->nseqs; i++) 
-    _msa->ss->col_tuples[0][i]='*';
+  {
+    _msa->ss->col_tuples[0][i] = '*';
+  }
   
-  for (ColumnIterator::ColumnMap::const_iterator it = cmap->begin(); it != cmap->end(); ++it) {
+  for (ColumnIterator::ColumnMap::const_iterator it = cmap->begin(); 
+       it != cmap->end(); ++it) 
+  {
     const Sequence *sequence = it->first;
     const Genome *genome = sequence->getGenome();
     int spec = hsh_get_int(_seqnameHash, genome->getName().c_str());
     if (spec < 0)
+    {
       continue;
+    }
     ColumnIterator::DNASet* dnaSet = it->second;
-    for (ColumnIterator::DNASet::const_iterator j = dnaSet->begin(); j != dnaSet->end(); ++j) {
+    for (ColumnIterator::DNASet::const_iterator j = dnaSet->begin(); 
+         j != dnaSet->end(); ++j) 
+    {
       DNAIteratorConstPtr dna = *j;
       char base = toupper(dna->getChar());
-      if (_msa->ss->col_tuples[0][spec]=='*')
+      if (_msa->ss->col_tuples[0][spec] == '*')
+      {
 	_msa->ss->col_tuples[0][spec] = base;
-      else {
-	if (_maskAllDups && _softMaskDups == 0) {  //hard mask, all dups
+      }
+      else 
+      {
+	if (_maskAllDups && _softMaskDups == 0) 
+        {  //hard mask, all dups
 	  return 0.0;  // duplication; mask this base
-	} else if (_maskAllDups) {  // soft mask, all dups
+	} 
+        else if (_maskAllDups) 
+        {  // soft mask, all dups
 	  _msa->ss->col_tuples[0][spec] = 'N';
-	} else if (_msa->ss->col_tuples[0][spec] != base) {
+	} 
+        else if (_msa->ss->col_tuples[0][spec] != base) 
+        {
 	  if (_softMaskDups == 0) 
+          {
 	    return 0.0;
-	  else _msa->ss->col_tuples[0][spec] ='N';
-	} else {
+          }
+	  else 
+          {
+            _msa->ss->col_tuples[0][spec] ='N';
+          }
+	} 
+        else 
+        {
 	  _msa->ss->col_tuples[0][spec] = base;
 	}
       }
     }
   }
-  for (int i=0; i < _msa->nseqs; i++) {
-    if (_msa->ss->col_tuples[0][i]=='*')
-      _msa->ss->col_tuples[0][i]='N';
+  
+  for (int i=0; i < _msa->nseqs; i++) 
+  {
+    if (_msa->ss->col_tuples[0][i] == '*')
+    {
+      _msa->ss->col_tuples[0][i] = 'N';
+    }
   }
   
   //finally, compute the score!
@@ -146,33 +343,47 @@ double halPhyloP::pval(const ColumnIterator::ColumnMap *cmap) {
   int sigfigs=4;  //same value used in phyloP code
   _mod->scale = 1;
   tm_set_subst_matrices(_mod);
-  null_lnl = col_compute_log_likelihood(_mod, _msa, 0, _colfitdata->fels_scratch[0]);
+  null_lnl = col_compute_log_likelihood(_mod, _msa, 0,
+                                        _colfitdata->fels_scratch[0]);
   vec_set(_colfitdata->params, 0, _colfitdata->init_scale);
-  opt_newton_1d(col_likelihood_wrapper_1d, &_colfitdata->params->data[0], _colfitdata,
-		&alt_lnl, sigfigs, _colfitdata->lb->data[0], _colfitdata->ub->data[0],
+  opt_newton_1d(col_likelihood_wrapper_1d, &_colfitdata->params->data[0],
+                _colfitdata,
+		&alt_lnl, sigfigs, _colfitdata->lb->data[0], 
+                _colfitdata->ub->data[0],
 		NULL, NULL, NULL);
   alt_lnl *= -1;
   this_scale = _colfitdata->params->data[0];
   delta_lnl = alt_lnl - null_lnl;
-  if (delta_lnl <= -0.01) { //shouldn't happen
+  
+  if (delta_lnl <= -0.01) 
+  { 
+    //shouldn't happen
     cerr << "got delta_lnl = " << delta_lnl << endl;
     throw hal_exception(string("ERROR col_lrts: delta_lnl < 0 "));
   }
   if (_mode == NNEUT || _mode == CONACC) 
+  {
     pval = chisq_cdf(2*delta_lnl, 1, FALSE);
+  }
   else 
+  {
     pval = half_chisq_cdf(2*delta_lnl, 1, FALSE);
+  }
   /* assumes 50:50 mix of chisq and point mass at zero, due to
      bounding of param */
   
   if (pval < 1e-20)
+  {
     pval = 1e-20;
+  }
   /* approx limit of eval of tail prob; pvals of 0 cause problems */
 
   pval = -log10(pval);
 
   if (_mode == CONACC && this_scale > 1)
+  {
     pval *= -1; /* mark as acceleration */
+  }
 
   // print out parameters for debugging
 //    cout << _msa->ss->col_tuples[0] << " " << _mod->scale << " " << alt_lnl << " " << null_lnl << " " << delta_lnl << " " << pval << " ";
--- phyloP/impl/halPhyloPMain.cpp
+++ phyloP/impl/halPhyloPMain.cpp
@@ -1,8 +1,10 @@
 /*
- * Copyright (C) 2012 by Glenn Hickey (hickey@soe.ucsc.edu)
+ * Copyright (C) 2013 by Glenn Hickey (hickey@soe.ucsc.edu) and 
+ * Melissa Jane Hubisz (Cornell University)
  *
  * Released under the MIT license, see LICENSE.txt
  */
+
 
 #include <cstdlib>
 #include <iostream>
@@ -13,39 +15,9 @@
 using namespace std;
 using namespace hal;
 
-/** This is basically copied from halAlignability.cpp but returns
- * the phyloP score rather than the count of alignments.
- *
- * Coordinates are always genome-relative by default (as opposed to 
- * sequence-relative).  The one exception is all methods within the
- * Sequence interface. 
- *
- * By default, all bases in the reference genome are scanned.  And all
- * other genomes are considered.  The --refSequence, --start, and 
- * --length options can limit the query to a subrange.  Note that unless
- * --refSequence is specified, --start is genome-relative (based on 
- * all sequences being concatenated together).
- *
- * Genomes to include are determined by genomes in the neutral model 
- * (.mod) file- this should be in the format outputted by phyloFit.
- *
- * There are a few options for dealing with duplications. The default
- * is dupMask=soft, dupType=ambiguous. This replaces any species base 
- * with an N if duplications cause uncertainty in the base for a 
- * particular alignment column. If dupType=all, then all duplications
- * are masked regardless of whether they cause uncertainty. If
- * dupMask=hard, then the entire column is masked (p-value returned is 1.0)
- */
-
-/** Print the phyloP wiggle for a subrange of a given sequence to
- * the output stream. */
-static void printSequence(ostream& outStream, halPhyloP *phyloP, 
-                          const Sequence* sequence, 
-                          hal_size_t start, hal_size_t length, hal_size_t step);
-
 /** If given genome-relative coordinates, map them to a series of 
  * sequence subranges */
-static void printGenome(ostream& outStream, halPhyloP *phyloP,
+static void printGenome(PhyloP *phyloP,
                         const Genome* genome, const Sequence* sequence,
                         hal_size_t start, hal_size_t length, hal_size_t step);
 
@@ -65,7 +37,7 @@ static CLParserPtr initParser()
   optionsParser->addOption("refSequence", "sequence name to export ("
                            "all sequences by default)", 
                            "\"\"");
-   optionsParser->addOption("start",
+  optionsParser->addOption("start",
                            "coordinate within reference genome (or sequence"
                            " if specified) to start at",
                            0);
@@ -119,7 +91,7 @@ int main(int argc, char** argv)
     step = optionsParser->getOption<hal_size_t>("step");
     dupType = optionsParser->getOption<string>("dupType");
     dupMask = optionsParser->getOption<string>("dupMask");
-
+    std::transform(dupMask.begin(), dupMask.end(), dupMask.begin(), ::tolower);
   }
   catch(exception& e)
   {
@@ -141,12 +113,6 @@ int main(int argc, char** argv)
     {
       throw hal_exception("input hal alignmenet is empty");
     }
-    
-
-    halPhyloP phyloPObj = halPhyloP(alignment, modPath);
-    halPhyloP *phyloP = &phyloPObj;
-    phyloP->setDupMask(dupMask);
-    phyloP->setDupType(dupType);
 
     /** Open the reference genome */
     const Genome* refGenome = NULL;
@@ -189,8 +155,10 @@ int main(int argc, char** argv)
       }
     }
     
-    printGenome(outStream, phyloP, refGenome, refSequence, start, length, 
-	        step);
+    PhyloP phyloP;
+    phyloP.init(alignment, modPath, &outStream, dupMask == "soft" , dupType);
+
+    printGenome(&phyloP, refGenome, refSequence, start, length, step);
     
   }
   catch(hal_exception& e)
@@ -207,105 +175,6 @@ int main(int argc, char** argv)
   return 0;
 }
 
-/** Given a Sequence (chromosome) and a (sequence-relative) coordinate
- * range, print the phyloP wiggle with respect to the genomes
- * in the target set */
-void printSequence(ostream& outStream, halPhyloP *phyloP, 
-		   const Sequence* sequence, 
-                   hal_size_t start, hal_size_t length, hal_size_t step)
-{
-  hal_size_t seqLen = sequence->getSequenceLength();
-  if (seqLen == 0)
-  {
-    return;
-  }
-  /** If the length is 0, we do from the start position until the end
-   * of the sequence */
-  if (length == 0)
-  {
-    length = seqLen - start;
-  }
-  hal_size_t last = start + length;
-  if (last > seqLen)
-  {
-    stringstream ss;
-    ss << "Specified range [" << start << "," << length << "] is"
-       << "out of range for sequence " << sequence->getName() 
-       << ", which has length " << seqLen;
-    throw (hal_exception(ss.str()));
-  }
-
-  const Genome* genome = sequence->getGenome();
-  string sequenceName = sequence->getName();
-  string genomeName = genome->getName();
-
-  /** The ColumnIterator is fundamental structure used in this example to
-   * traverse the alignment.  It essientially generates the multiple alignment
-   * on the fly according to the given reference (in this case the target
-   * sequence).  Since this is the sequence interface, the positions
-   * are sequence relative.  Note that we must specify the last position
-   * in advance when we get the iterator.  This will limit it following
-   * duplications out of the desired range while we are iterating. */
-  hal_size_t pos = start;
-  ColumnIteratorConstPtr colIt = sequence->getColumnIterator(&phyloP->_targetSet,
-                                                             0, pos,
-                                                             last - 1,
-                                                             true);
-  // note wig coordinates are 1-based for some reason so we shift to right
-  outStream << "fixedStep chrom=" << sequenceName << " start=" << start + 1
-            << " step=" << step << "\n";
-
-  // set float precision to 3 places to be consistent with non-hal phyloP
-  outStream.setf(ios::fixed, ios::floatfield);
-  outStream.precision(3);
-  
-  /** Since the column iterator stores coordinates in Genome coordinates
-   * internally, we have to switch back to genome coordinates.  */
-  // convert to genome coordinates
-  pos += sequence->getStartPosition();
-  last += sequence->getStartPosition();
-  while (pos <= last)
-  {
-    /** ColumnIterator::ColumnMap maps a Sequence to a list of bases
-     * the bases in the map form the alignment column.  Some sequences
-     * in the map can have no bases (for efficiency reasons) */ 
-    const ColumnIterator::ColumnMap* cmap = colIt->getColumnMap();
-    double pval = phyloP->pval(cmap);
-
-    outStream << pval << '\n';
-    
-    /** lastColumn checks if we are at the last column (inclusive)
-     * in range.  So we need to check at end of iteration instead
-     * of beginning (which would be more convenient).  Need to 
-     * merge global fix from other branch */
-    if (colIt->lastColumn() == true)
-    {
-      break;
-    }
-
-    pos += step;    
-    if (step == 1)
-    {
-      /** Move the iterator one position to the right */
-      colIt->toRight();
-      
-      /** This is some tuning code that will probably be hidden from 
-       * the interface at some point.  It is a good idea to use for now
-       * though */
-      // erase empty entries from the column.  helps when there are 
-      // millions of sequences (ie from fastas with lots of scaffolds)
-      if (pos % 1000 == 0)
-      {
-        colIt->defragment();
-      }
-    }
-    else
-    {
-      /** Reset the iterator to a non-contiguous position */
-      colIt->toSite(pos, last);
-    }
-  }
-}
 
 /** Map a range of genome-level coordinates to potentially multiple sequence
  * ranges.  For example, if a genome contains two chromosomes ChrA and ChrB,
@@ -315,13 +184,13 @@ void printSequence(ostream& outStream, halPhyloP *phyloP,
  * for the hal::Sequence interface.  We can convert between the two by 
  * adding or subtracting the sequence start position (in the example it woudl
  * be 0 for ChrA and 500 for ChrB) */
-void printGenome(ostream& outStream, halPhyloP *phyloP, 
+void printGenome(PhyloP *phyloP, 
                  const Genome* genome, const Sequence* sequence,
                  hal_size_t start, hal_size_t length, hal_size_t step)
 {
   if (sequence != NULL)
   {
-    printSequence(outStream, phyloP, sequence, start, length, step);
+    phyloP->processSequence(sequence, start, length, step);
   }
   else
   {
@@ -354,7 +223,7 @@ void printGenome(ostream& outStream, halPhyloP *phyloP,
         hal_size_t readStart = seqStart >= start ? 0 : start - seqStart;
         hal_size_t readLen = min(seqLen - readStart, length);
 
-        printSequence(outStream, phyloP, sequence, readStart, readLen, step);
+        phyloP->processSequence(sequence, readStart, readLen, step);
         runningLength += readLen;
       }
     }
