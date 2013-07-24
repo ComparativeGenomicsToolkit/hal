ad9d9beecef158954de9e584ce6c8b60489d3ddb
Joel Armstrong <jcarmstr@ucsc.edu>
2013-07-23 17:33:49 -0700

    Append subtree utility

---
+++ modify/halAppendSubtree.cpp
@@ -0,0 +1,79 @@
+#include "hal.h"
+
+using namespace std;
+using namespace hal;
+
+static CLParserPtr initParser()
+{
+  CLParserPtr optionsParser = hdf5CLParserInstance(true);
+  optionsParser->addArgument("mainFile", "destination tree");
+  optionsParser->addArgument("appendFile", "alignment containing the tree to be"
+                             " appended");
+  optionsParser->addArgument("bridgeFile", "alignment containing parent,"
+                             " subtree root, and its future siblings, if any");
+  optionsParser->addArgument("parentName", "node to be added to");
+  optionsParser->addArgument("rootName", "name of subtree root");
+  optionsParser->addArgument("branchLength", "branch length");
+  return optionsParser;
+}
+
+void addSubtree(AlignmentPtr mainAlignment, AlignmentConstPtr appendAlignment, 
+                string currNode)
+{
+  Genome *outGenome = mainAlignment->openGenome(currNode);
+  const Genome *inGenome = appendAlignment->openGenome(currNode);
+  inGenome->copy(outGenome);
+  vector<string> children = appendAlignment->getChildNames(currNode);
+  for (size_t i = 0; i < children.size(); i++) {
+    mainAlignment->addLeafGenome(children[i], currNode,
+                                 appendAlignment->getBranchLength(currNode,
+                                                                   children[i]));
+    addSubtree(mainAlignment, appendAlignment, children[i]);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  CLParserPtr optParser = initParser();
+  string mainPath, appendPath, bridgePath, parentName, rootName;
+  double branchLength;
+  try {
+    optParser->parseOptions(argc, argv);
+    mainPath = optParser->getArgument<string>("mainFile");
+    appendPath = optParser->getArgument<string>("appendFile");
+    bridgePath = optParser->getArgument<string>("bridgeFile");
+    parentName = optParser->getArgument<string>("parentName");
+    rootName = optParser->getArgument<string>("rootName");
+    branchLength = optParser->getArgument<double>("branchLength");
+  } catch (exception &e) {
+    optParser->printUsage(cerr);
+    return 1;
+  }
+  AlignmentPtr mainAlignment = openHalAlignment(mainPath, optParser);
+  AlignmentConstPtr appendAlignment = openHalAlignment(appendPath, optParser);
+  AlignmentConstPtr bridgeAlignment = openHalAlignment(bridgePath,
+                                                         optParser);
+
+  mainAlignment->addLeafGenome(rootName, parentName, branchLength);
+  addSubtree(mainAlignment, appendAlignment, rootName);
+
+  // Need proper bottom segments for parent genome
+  Genome *mainParentGenome = mainAlignment->openGenome(parentName);
+  const Genome *bridgeParentGenome = bridgeAlignment->openGenome(parentName);
+  bridgeParentGenome->copyBottomDimensions(mainParentGenome);
+  bridgeParentGenome->copyBottomSegments(mainParentGenome);
+
+  // And top segments for its children
+  vector<string> children = bridgeAlignment->getChildNames(parentName);
+  for (size_t i = 0; i < children.size(); i++) {
+    Genome *mainChildGenome = mainAlignment->openGenome(children[i]);
+    const Genome *bridgeChildGenome = bridgeAlignment->openGenome(children[i]);
+    bridgeChildGenome->copyTopDimensions(mainChildGenome);
+    bridgeChildGenome->copyTopSegments(mainChildGenome);
+  }
+
+  mainAlignment->close();
+  appendAlignment->close();
+  bridgeAlignment->close();
+  return 0;
+}
