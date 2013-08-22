22d2ba9fce8f8dfc81478cc491e28670f9925a2d
Joel Armstrong <jcarmstr@ucsc.edu>
2013-08-22 16:02:50 -0700

    Add "merge" option to halAppendSubtree
    Using the option will merge the appended node and the node which is
    appended to.
    
    This makes halAddToNode unnecessary (this is more general) so I
    removed it.

--- modify/halAppendSubtree.cpp
+++ modify/halAppendSubtree.cpp
@@ -10,13 +10,18 @@ static CLParserPtr initParser()
   optionsParser->addArgument("mainFile", "destination tree");
   optionsParser->addArgument("appendFile", "alignment containing the tree to be"
                              " appended");
-  optionsParser->addArgument("bridgeFile", "alignment containing parent,"
-                             " subtree root, and its future siblings, if any");
   optionsParser->addArgument("parentName", "node to be added to");
   optionsParser->addArgument("rootName", "name of subtree root");
-  optionsParser->addArgument("branchLength", "branch length");
+  optionsParser->addOption("bridgeFile", "alignment containing parent,"
+                           " subtree root, and its future siblings, if any "
+                           "(required if not merging appended and appendee "
+                           "nodes)", "");
+  optionsParser->addOption("branchLength", "branch length between appended and "
+                           "appendee nodes", 0.0);
   optionsParser->addOptionFlag("noMarkAncestors", "don't mark ancestors for"
                                " update", false);
+  optionsParser->addOptionFlag("merge", "merge appended root and node that is appended to",
+                               false);
   return optionsParser;
 }
 
@@ -25,14 +30,21 @@ void addSubtree(AlignmentPtr mainAlignment, AlignmentConstPtr appendAlignment,
 {
   Genome *outGenome = mainAlignment->openGenome(currNode);
   const Genome *inGenome = appendAlignment->openGenome(currNode);
-  inGenome->copy(outGenome);
   vector<string> children = appendAlignment->getChildNames(currNode);
   for (size_t i = 0; i < children.size(); i++) {
-    mainAlignment->addLeafGenome(children[i], currNode,
-                                 appendAlignment->getBranchLength(currNode,
-                                                                   children[i]));
+    if (mainAlignment->openGenome(children[i]) != NULL) {
+      // Special case for a node that is being merged; we don't want
+      // to append children that already exist.
+      continue;
+    }
+    Genome *mainChildGenome = mainAlignment->addLeafGenome(children[i], currNode, appendAlignment->getBranchLength(currNode, children[i]));
+    const Genome *appendChildGenome = appendAlignment->openGenome(children[i]);
+    appendChildGenome->copy(mainChildGenome);
     addSubtree(mainAlignment, appendAlignment, children[i]);
   }
+  inGenome->copyBottomDimensions(outGenome);
+  inGenome->copyBottomSegments(outGenome);
+  outGenome->fixParseInfo();
 }
 
 int main(int argc, char *argv[])
@@ -41,25 +53,43 @@ int main(int argc, char *argv[])
   string mainPath, appendPath, bridgePath, parentName, rootName;
   double branchLength;
   bool noMarkAncestors;
+  bool merge;
   try {
     optParser->parseOptions(argc, argv);
     mainPath = optParser->getArgument<string>("mainFile");
     appendPath = optParser->getArgument<string>("appendFile");
-    bridgePath = optParser->getArgument<string>("bridgeFile");
+    bridgePath = optParser->getOption<string>("bridgeFile");
     parentName = optParser->getArgument<string>("parentName");
     rootName = optParser->getArgument<string>("rootName");
-    branchLength = optParser->getArgument<double>("branchLength");
+    branchLength = optParser->getOption<double>("branchLength");
     noMarkAncestors = optParser->getFlag("noMarkAncestors");
+    merge = optParser->getFlag("merge");
   } catch (exception &e) {
     optParser->printUsage(cerr);
     return 1;
   }
   AlignmentPtr mainAlignment = openHalAlignment(mainPath, optParser);
   AlignmentConstPtr appendAlignment = openHalAlignment(appendPath, optParser);
-  AlignmentConstPtr bridgeAlignment = openHalAlignment(bridgePath,
-                                                         optParser);
+  AlignmentConstPtr bridgeAlignment;
 
-  mainAlignment->addLeafGenome(rootName, parentName, branchLength);
+  if (!merge) {
+    if (bridgePath == "") {
+      throw hal_exception("need a bridge alignment if not merging nodes");
+    }
+    bridgeAlignment = openHalAlignment(bridgePath, optParser);
+    Genome *mainAppendedRoot = mainAlignment->addLeafGenome(rootName,
+                                                            parentName,
+                                                            branchLength);
+    const Genome *appendAppendedRoot = appendAlignment->openGenome(rootName);
+    appendAppendedRoot->copy(mainAppendedRoot);
+  } else {
+    // the bridge alignment is equivalent to the append alignment in this case
+    // (the append alignment will contain at least all the information that
+    // the bridge alignment would)
+    bridgeAlignment = appendAlignment;
+    assert(parentName == rootName);
+    assert(branchLength == 0.0);
+  }
   addSubtree(mainAlignment, appendAlignment, rootName);
 
   // Need proper bottom segments for parent genome
@@ -67,6 +97,7 @@ int main(int argc, char *argv[])
   const Genome *bridgeParentGenome = bridgeAlignment->openGenome(parentName);
   bridgeParentGenome->copyBottomDimensions(mainParentGenome);
   bridgeParentGenome->copyBottomSegments(mainParentGenome);
+  mainParentGenome->fixParseInfo();
 
   // And top segments for its children
   vector<string> children = bridgeAlignment->getChildNames(parentName);
@@ -75,12 +106,16 @@ int main(int argc, char *argv[])
     const Genome *bridgeChildGenome = bridgeAlignment->openGenome(children[i]);
     bridgeChildGenome->copyTopDimensions(mainChildGenome);
     bridgeChildGenome->copyTopSegments(mainChildGenome);
+    mainChildGenome->fixParseInfo();
   }
+
   if (!noMarkAncestors) {
     markAncestorsForUpdate(mainAlignment, rootName);
   }
   mainAlignment->close();
   appendAlignment->close();
-  bridgeAlignment->close();
+  if (!merge) {
+    bridgeAlignment->close();
+  }
   return 0;
 }
