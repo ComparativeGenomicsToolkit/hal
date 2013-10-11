749f671e771cc70b2c4e0a047da94c5a9472142d
Glenn Hickey <hickey@soe.ucsc.edu>
2013-10-10 18:51:54 -0700

    update script

--- chain/test/timing.sh
+++ chain/test/timing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash                                                                    
 
-binSize=100000
+binSize=1000000
 reps=1000
 ref=reference
 tgts=EscherichiaColiWUid162011,EscherichiaColiKo11flUid162099,EscherichiaColiKo11flUid52593,ShigellaSonnei53gUid84383
@@ -10,9 +10,11 @@ lodPath=http://hgwdev.cse.ucsc.edu/~nknguyen/ecoli/hub/pangenome-dups/lod.txt
 halPath=http://hgwdev.cse.ucsc.edu/~nknguyen/ecoli/hub/pangenome-dups/out.hal
 udcPath=/hive/users/hickey/udcTemp
 outPath=/hive/users/hickey/timing
+seed=2323
+python=/cluster/home/jcarmstr/Python-2.7/python
 
-./blockVizBenchmark.py ${lodPath} ${ref} ${refChr} ${len} ${tgts} --udc ${udcPath} --reps ${reps} --binSize ${binSize} > ${outPath}/ecoli.csv
+${python} ./blockVizBenchmark.py ${lodPath} ${ref} ${refChr} ${len} ${tgts} --udc ${udcPath} --reps ${reps} --binSize ${binSize} --seed ${seed} > ${outPath}/ecoli.csv
 
-./blockVizBenchmark.py ${lodPath} ${ref} ${refChr} ${len} ${tgts} --udc ${udcPath} --reps ${reps} --binSize ${binSize} --zapUdc > ${outPath}/ecoli_noudc.csv
+${python} ./blockVizBenchmark.py ${lodPath} ${ref} ${refChr} ${len} ${tgts} --udc ${udcPath} --reps ${reps} --binSize ${binSize} --zapUdc --seed ${seed} > ${outPath}/ecoli_noudc.csv
 
-#./blockVizBenchmark.py ${halPath} ${ref} ${refChr} ${len} ${tgts} --udc ${udcPath} --reps ${reps} --binSize ${binSize} > ${outPath}/ecoli_nolod.csv
+${python} ./blockVizBenchmark.py ${halPath} ${ref} ${refChr} ${len} ${tgts} --udc ${udcPath} --reps ${reps} --binSize ${binSize} --seed ${seed} > ${outPath}/ecoli_nolod.csv
