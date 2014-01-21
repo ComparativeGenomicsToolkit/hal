124bb2148dbd76e7e142b6f6d55a5f61f288eea1
Joel Armstrong <jcarmstr@ucsc.edu>
2014-01-20 17:59:19 -0800

    parallelize contiguous regions script

--- analysis/halContiguousRegions.py
+++ analysis/halContiguousRegions.py
@@ -2,21 +2,97 @@
 # won't work in the general case right now
 # (i.e. inputs need to be single copy & positive query strand)
 import sys
+import os
 import argparse
 from sonLib import bioio
+from sonLib.bioio import getTempFile
 from operator import itemgetter
 from collections import defaultdict
+from jobTree.scriptTree.target import Target
+from jobTree.scriptTree.stack import Stack
+from sonLib.bioio import logger
+from sonLib.bioio import setLoggingFromOptions
+class Setup(Target):
+    def __init__(self, args):
+        Target.__init__(self)
+        self.args = args
+
+    def run(self):
+        numLines = self.numLinesInFile(self.args.bedFile)
+        slices = []
+        sliceNum = self.args.sliceNum
+        if numLines > self.args.sliceNum:
+            step = numLines/self.args.sliceNum
+            count = 0
+            for i in xrange(sliceNum):
+                slices.append((count, count + step))
+                count += step
+            slices[-1] = (slices[-1][0], slices[-1][1] + numLines % self.args.sliceNum)
+        else:
+            sliceNum = numLines
+            slices = [1]*sliceNum
+
+        sliceOutputs = []
+        for i in xrange(sliceNum):
+            slice = slices[i]
+            sliceOut = getTempFile(rootDir=self.getGlobalTempDir())
+            sliceOutputs.append(sliceOut)
+            self.addChildTarget(RunContiguousRegions(self.args, slice,
+                                                     sliceOut))
+        self.setFollowOnTarget(WriteToOutput(self.args, sliceOutputs))
+
+    def numLinesInFile(self, fileName):
+        n = 0
+        for line in open(fileName):
+            n += 1
+        return n
+
+class RunContiguousRegions(Target):
+    def __init__(self, args, slice, sliceOut):
+        Target.__init__(self)
+        self.args = args
+        self.slice = slice
+        self.sliceOut = sliceOut
+
+    def run(self):
+        contiguousRegions = ContiguousRegions(self.args.alignment,
+                                              self.args.srcGenome,
+                                              self.args.destGenome,
+                                              self.args.maxGap,
+                                              self.getGlobalTempDir())
+        startLineNum = self.slice[0]
+        endLineNum = self.slice[1]
+        outFile = open(self.sliceOut, 'w')
+        for line in contiguousRegions.getContiguousLines(self.args.bedFile,
+                                                         startLineNum,
+                                                         endLineNum):
+            outFile.write(line)
+
+class WriteToOutput(Target):
+    def __init__(self, args, sliceFiles):
+        Target.__init__(self)
+        self.args = args
+        self.sliceFiles = sliceFiles
+
+    def run(self):
+        outFile = open(self.args.outFile, 'w')
+        for filename in self.sliceFiles:
+            for line in open(filename):
+                outFile.write(line)
 
 class ContiguousRegions:
-    def __init__(self, alignment, srcGenome, destGenome, maxGap):
+    def __init__(self, alignment, srcGenome, destGenome, maxGap, tempRoot):
         self.alignment = alignment
         self.srcGenome = srcGenome
         self.destGenome = destGenome
         self.maxGap = maxGap
+        self.tempRoot = tempRoot
 
     def liftover(self, bedLine):
-        tempSrc = bioio.getTempFile("ContiguousRegions.tempSrc.bed")
-        tempDest = bioio.getTempFile("ContiguousRegions.tempDest.psl")
+        tempSrc = getTempFile("ContiguousRegions.tempSrc.bed",
+                                    rootDir=self.tempRoot)
+        tempDest = getTempFile("ContiguousRegions.tempDest.psl",
+                                     rootDir=self.tempRoot)
         open(tempSrc, 'w').write("%s\n" % bedLine)
         cmd = "halLiftover --outPSL %s %s %s %s %s" % (self.alignment,
                                                        self.srcGenome,
@@ -25,6 +101,8 @@ class ContiguousRegions:
                                                        tempDest)
         bioio.system(cmd)
         pslLines = open(tempDest).read().split("\n")
+        os.remove(tempSrc)
+        os.remove(tempDest)
         pslLines = map(lambda x: x.split(), pslLines)
         # dict is to keep blocks separated by target sequence
         qBlocks = defaultdict(list)
@@ -121,8 +199,12 @@ class ContiguousRegions:
 
         return True
 
-    def getContiguousLines(self, bedPath):
-        for line in open(bedPath):
+    def getContiguousLines(self, bedPath, startLineNum=0, endLineNum=-1):
+        for lineNum, line in enumerate(open(bedPath)):
+            if lineNum < startLineNum:
+                continue
+            elif lineNum >= endLineNum:
+                break
             # can't handle bed12
             assert(len(line.split()) >= 3 and len(line.split()) < 12)
             if self.isContiguousInTarget(line):
@@ -134,16 +216,20 @@ def main():
     parser.add_argument("srcGenome", help="Reference genome")
     parser.add_argument("bedFile", help="Bed file (in ref coordinates)")
     parser.add_argument("destGenome", help="Genome to check contiguity in")
+    parser.add_argument("outFile", help="Output BED file")
     parser.add_argument("--maxGap", help="maximum gap size to accept", 
                         default=10, type=int)
+    parser.add_argument("--sliceNum", help="number of slices to create",
+                        type=int, default=1)
+    Stack.addJobTreeOptions(parser)
     args = parser.parse_args()
+    setLoggingFromOptions(args)
+    result = Stack(Setup(args)).startJobTree(args)
+    if result:
+        raise RuntimeError("Jobtree has failed jobs.")
 
-    contiguousRegions = ContiguousRegions(args.alignment, args.srcGenome,
-                                          args.destGenome, args.maxGap)
-    
-    for line in contiguousRegions.getContiguousLines(args.bedFile):
-        print line
     return 0
     
 if __name__ == '__main__':
+    from hal.analysis.halContiguousRegions import *
     sys.exit(main())
