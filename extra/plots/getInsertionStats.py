ee97b171fbe1056d538d7018a8ae12ab8700121c
Joel Armstrong <jcarmstr@ucsc.edu>
2014-08-31 21:01:53 -0700

    add masking and completely-unaligned sequence info

--- extra/plots/getInsertionStats.py
+++ extra/plots/getInsertionStats.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
+"""Get a TSV of insertion size, length, seq, genome sampled from a HAL
+file."""
 from argparse import ArgumentParser
-from sonLib.bioio import popenCatch, system, getTempFile
+from sonLib.bioio import popenCatch, system, getTempFile, fastaRead
 from jobTree.scriptTree.stack import Stack
 from jobTree.scriptTree.target import Target
+
+def countMaskedBases(string):
+    ret = 0
+    for char in string:
+        if char != 'A' and char != 'C' and char != 'T' and char != 'G':
+            ret += 1
+    return ret
 
 class Setup(Target):
     def __init__(self, opts):
@@ -15,11 +24,11 @@ class Setup(Target):
         for genome in genomes:
             tempOutput = getTempFile(rootDir=self.getGlobalTempDir())
             outputs.append(tempOutput)
-            self.addChildTarget(ExtractInsertions(self.opts.halPath, genome, tempOutput, self.opts.samplePerGenome, self.opts.samples, self.opts.noGaps))
+            self.addChildTarget(ExtractInsertions(self.opts.halPath, genome, tempOutput, self.opts.samplePerGenome, self.opts.samples, self.opts.noGaps, self.opts.entirelyInsertedSequencesPath))
         self.setFollowOnTarget(ReduceOutputs(outputs, self.opts.output, not self.opts.samplePerGenome, self.opts.samples))
 
 class ExtractInsertions(Target):
-    def __init__(self, halPath, genome, output, doSampling, numSamples, removeGaps):
+    def __init__(self, halPath, genome, output, doSampling, numSamples, removeGaps, entirelyInsertedSequencePath):
         Target.__init__(self)
         self.halPath = halPath
         self.genome = genome
@@ -27,10 +36,40 @@ class ExtractInsertions(Target):
         self.doSampling = doSampling
         self.numSamples = numSamples
         self.removeGaps = removeGaps
+        self.entirelyInsertedSequencePath = entirelyInsertedSequencePath
+
+    def getFastaDict(self):
+        temp = getTempFile(rootDir=self.getGlobalTempDir())
+        system("hal2fasta %s %s > %s" % (self.halPath, self.genome, temp))
+        ret = {}
+        for header, seq in fastaRead(temp):
+            ret[header] = seq
+        return ret
+
+    def logEntirelyInsertedSequences(self, fastaDict, chromSizes, insertionBed):
+        outFile = open(self.entirelyInsertedSequencePath, 'a')
+        for line in open(insertionBed):
+            fields = line.split()
+            if len(fields) >= 3:
+                seq = fields[0]
+                start = int(fields[1])
+                end = int(fields[2])
+                if end - start == chromSizes[seq]:
+                    dna = fastaDict[seq][start:end]
+                    maskedBases = countMaskedBases(dna)
+                    outFile.write("%d\t%s\t%s\t%s\n" % (end - start, self.genome, seq, maskedBases))
 
     def run(self):
+        fastaDict = self.getFastaDict()
+        chromSizes = dict([(x[0], len(x[1])) for x in fastaDict.items()])
+
         insertionBed = getTempFile(rootDir=self.getGlobalTempDir())
         system("halAlignedExtract --complement %s %s > %s" % (self.halPath, self.genome, insertionBed))
+
+        if self.entirelyInsertedSequencePath is not None:
+            # Look for insertions that cover an entire sequence
+            self.logEntirelyInsertedSequences(fastaDict, chromSizes, insertionBed)
+
         if self.doSampling:
             # Sample per-genome instead of overall
             temp = getTempFile(rootDir=self.getGlobalTempDir())
@@ -43,17 +82,14 @@ class ExtractInsertions(Target):
                 seq = fields[0]
                 start = int(fields[1])
                 end = int(fields[2])
+                dna = fastaDict[seq][start:end]
                 if self.removeGaps:
-                    # Get rid of gaps. This will make everything slow
-                    # as hell and we should really load the sequence
-                    # once and then query it. but whatever
-                    dna = popenCatch("hal2fasta %s %s --sequence %s --start %d --length %d" % (self.halPath, seq, start, end - start))
-                    # remove header
-                    dna = dna.split("\n")[1:]
+                    # Get rid of gaps.
                     if 'N' in dna or 'n' in dna:
                         # Found a gap
                         continue
-                outFile.write("%d\t%s\t%s\n" % (end - start, self.genome, seq))
+                maskedBases = countMaskedBases(dna)
+                outFile.write("%d\t%s\t%s\t%s\n" % (end - start, self.genome, seq, maskedBases))
         outFile.close()
 
 class ReduceOutputs(Target):
@@ -77,12 +113,13 @@ class ReduceOutputs(Target):
 
 if __name__ == '__main__':
     from getInsertionStats import * # required for jobTree
-    parser = ArgumentParser()
+    parser = ArgumentParser(description=__doc__)
     parser.add_argument('halPath', help='hal file')
-    parser.add_argument('output', help='output tsv')
-    parser.add_argument('--samples', help='number of samples per genome', default=10000, type=int)
+    parser.add_argument('output', help='output tsv for a sample of insertions')
+    parser.add_argument('--samples', help='number of samples', default=10000, type=int)
     parser.add_argument('--samplePerGenome', action="store_true", help="sample n samples per genome instead of n samples overall")
     parser.add_argument('--noGaps', action="store_true", help="remove any sequences with Ns in them")
+    parser.add_argument('--entirelyInsertedSequencesPath', help="only store information about any sequences that are completely unaligned")
     Stack.addJobTreeOptions(parser)
     opts = parser.parse_args()
     Stack(Setup(opts)).startJobTree(opts)
