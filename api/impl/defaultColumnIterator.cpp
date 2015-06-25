bd2109a75747ece6ae24180e5ba54d41860aad04
Joel Armstrong <jcarmstr@ucsc.edu>
2015-06-25 15:50:26 -0700

    prevent getting an incorrect tree from an onlyOrthologs column

--- api/impl/defaultColumnIterator.cpp
+++ api/impl/defaultColumnIterator.cpp
@@ -605,6 +605,12 @@ static void buildTreeR(BottomSegmentIteratorConstPtr botIt, stTree *tree)
 // Build a gene-tree from a column iterator.
 stTree *DefaultColumnIterator::getTree() const
 {
+  if (_onlyOrthologs) {
+    // Because the tree-finding code goes all the way up the column
+    // tree and I'm too lazy to make it smarter.
+    throw hal_exception("Cannot get the tree for a column iterator "
+                        "which only displays orthologs.")
+  }
   if (_tree != NULL) {
     return _tree;
   } else {
