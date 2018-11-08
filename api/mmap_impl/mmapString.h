27c91dd1c90e569a37b4babdf720ba9c3bbfef38
Joel Armstrong <jcarmstr@ucsc.edu>
2018-11-07 20:16:23 -0800

    String specialization of generic MMapArray

---
+++ api/mmap_impl/mmapString.h
@@ -0,0 +1,28 @@
+#ifndef _MMAP_STRING_H
+#define _MMAP_STRING_H
+#include "mmapArray.h"
+#include <iostream>
+namespace hal {
+class MMapString : public MMapArray<char> {
+    public:
+    MMapString(MMapAlignment *alignment, const std::string &string) : MMapArray(alignment), _string(string) { set(_string); };
+    MMapString(MMapAlignment *alignment, size_t offset) : MMapArray(alignment, offset) { read(); };
+    const char *c_str() { return getSlice(0, getLength() - 1); }
+    const std::string &get() { return _string; }
+    size_t set(const std::string &string) {
+        _string = string;
+        setLength(string.size() + 1);
+        for (size_t i = 0; i < string.size(); i++) {
+            *(this->operator[](i)) = string[i];
+        }
+        *(this->operator[](string.size())) = '\0';
+        return getOffset();
+    }
+    private:
+    void read() {
+        _string = c_str();
+    };
+    std::string _string;
+};
+}
+#endif
