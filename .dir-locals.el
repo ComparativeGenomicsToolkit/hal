b5b4d24a39306f1aaee75d06b7d2d6a7bc59fe83
Mark Diekhans <markd@soe.ucsc.edu>
2019-05-02 18:53:59 -0700

    added some stuff related to reformatting

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+; TMP disabled
+; ((c++-mode
+;   (##)
+;   (c-file-style . "google")
+;   (c-basic-offset . 4)
+;   (c-file-offsets
+;    (innamespace . 4))))
+; 
