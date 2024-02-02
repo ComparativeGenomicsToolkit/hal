087c0cab24618e728195a7e5f7a8cef99c41b3f8
Glenn Hickey <glenn.hickey@gmail.com>
2024-02-02 13:38:33 -0500

    new option --bySequence to break down halCoverage by ref sequence

--- stats/impl/halCoverage.cpp
+++ stats/impl/halCoverage.cpp
@@ -11,16 +11,20 @@ int main(int argc, char **argv) {
     optionsParser.addArgument("refGenome", "genome to calculate coverage on");
     optionsParser.addOption("numSamples", "Number of bases to sample when calculating coverage", 1000000);
     optionsParser.addOption("seed", "Random seed (integer)", 0);
+    optionsParser.addOptionFlag("bySequence", "provide coverage breakdown by sequence in reference genome", false);
+    
     string path;
     string refGenome;
     hal_size_t numSamples;
     int64_t seed;
+    bool bySequence;
     try {
         optionsParser.parseOptions(argc, argv);
         path = optionsParser.getArgument<string>("halFile");
         refGenome = optionsParser.getArgument<string>("refGenome");
         numSamples = optionsParser.getOption<hal_size_t>("numSamples");
         seed = optionsParser.getOption<int64_t>("seed");
+        bySequence = optionsParser.getFlag("bySequence");
     } catch (exception &e) {
         cerr << e.what() << endl;
         optionsParser.printUsage(cerr);
@@ -38,24 +42,39 @@ int main(int argc, char **argv) {
     const Genome *ref = alignment->openGenome(refGenome);
     vector<const Genome *> leafGenomes = getLeafGenomes(alignment.get());
 
-    map<const Genome *, vector<hal_size_t>> coverage;
-    for (size_t i = 0; i < leafGenomes.size(); i++) {
-        coverage.insert(make_pair(leafGenomes[i], vector<hal_size_t>()));
+    // null key means whole genome here
+    map<const Sequence*, map<const Genome*, vector<hal_size_t>>> coverage_by_sequence;
+    vector<const Sequence*> sequences = {NULL};
+    if (bySequence) {
+        for (SequenceIteratorPtr si = ref->getSequenceIterator(); !si->atEnd(); si->toNext()) {
+            sequences.push_back(si->getSequence());
+        }
+    }    
+    for (const Sequence* sequence : sequences) {
+        map<const Genome*, vector<hal_size_t>>& coverage = coverage_by_sequence[sequence];
+        for (size_t i = 0; i < leafGenomes.size(); i++) {
+            coverage.insert(make_pair(leafGenomes[i], vector<hal_size_t>()));
+        }
     }
 
     hal_size_t maxDepth = 0;
 
+    map<const Genome*, vector<hal_size_t>>& genome_coverage = coverage_by_sequence[NULL];
     for (hal_size_t i = 0; i < numSamples; i++) {
         // Sample (with replacement) a random position in the reference genome.
         hal_index_t pos = st_randomInt64(0, ref->getSequenceLength());
         SegmentIteratorPtr refSeg = ref->getTopSegmentIterator();
         refSeg->toSite(pos, true);
         assert(refSeg->getLength() == 1);
+        map<const Genome*, vector<hal_size_t>>* sequence_coverage = NULL;
+        if (bySequence) {
+            sequence_coverage = &coverage_by_sequence[refSeg->getSequence()];
+        }
         for (size_t j = 0; j < leafGenomes.size(); j++) {
             const Genome *leafGenome = leafGenomes[j];
             MappedSegmentSet segments;
             halMapSegmentSP(refSeg, segments, leafGenome, NULL, true, 0, NULL, NULL);
-            vector<hal_size_t> &histogram = coverage[leafGenome];
+            vector<hal_size_t> &histogram = genome_coverage[leafGenome];
             hal_size_t depth = segments.size();
             if (depth > maxDepth) {
                 maxDepth = depth;
@@ -66,6 +85,15 @@ int main(int argc, char **argv) {
             for (size_t k = 0; k < depth; k++) {
                 histogram[k] += 1;
             }
+            if (sequence_coverage) {
+                vector<hal_size_t> &seq_histogram = sequence_coverage->at(leafGenome);
+                if (seq_histogram.size() < depth) {
+                    seq_histogram.resize(depth, 0);
+                }
+                for (size_t k = 0; k < depth; k++) {
+                    seq_histogram[k] += 1;
+                }
+            }
         }
     }
 
@@ -75,17 +103,23 @@ int main(int argc, char **argv) {
     }
     cout << endl;
 
-    for (map<const Genome *, vector<hal_size_t>>::iterator it = coverage.begin(); it != coverage.end(); it++) {
-        string name = it->first->getName();
-        cout << name;
-        vector<hal_size_t> histogram = it->second;
-        for (hal_size_t i = 0; i < maxDepth; i++) {
-            if (i < histogram.size()) {
-                cout << ", " << histogram[i];
-            } else {
-                cout << ", 0";
-            }
+    for (const Sequence* refseq : sequences) {
+        if (refseq != NULL) {
+            cerr << "\nCoverage on " << refseq->getName() << endl;
         }
-        cout << endl;
+        map<const Genome*, vector<hal_size_t>>& coverage = coverage_by_sequence[refseq];
+        for (map<const Genome *, vector<hal_size_t>>::iterator it = coverage.begin(); it != coverage.end(); it++) {
+            string name = it->first->getName();
+            cout << name;
+            vector<hal_size_t> histogram = it->second;
+            for (hal_size_t i = 0; i < maxDepth; i++) {
+                if (i < histogram.size()) {
+                    cout << ", " << histogram[i];
+                } else {
+                    cout << ", 0";
+                }
+            }
+            cout << endl;
+        }
     }
 }
